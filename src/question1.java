ccb8f61cb1a74b44a4c0d5aa4f153470dd3de55c
ebuchanan2 <Buchanan-E2@ulster.ac.uk>
2024-09-25 15:39:46 +0100

    finished question 1

--- src/question1.java
+++ src/question1.java
@@ -1,12 +1,32 @@
 import uulib.*;
+import java.util.*;
 public class question1 {
     public static void main(String[] args){
+        //Initialise scanner
+        Scanner sc = new Scanner(System.in);
+
         //Initialise variables
-        char initial = 'J';
-        String surname = "Smith";
-        int age = 25;
-        String isEmployed = "employed";
-        int salary = 21000;
+        String initial;
+        String surname;
+        int age;
+        String isEmployed;
+        int salary;
+
+        //Prompts user to input variables one by one
+        System.out.println("Enter initial: ");
+        initial = sc.nextLine();
+
+        System.out.println("Enter surname: ");
+        surname = sc.nextLine();
+
+        System.out.println("Enter if employed: ");
+        isEmployed = sc.nextLine();
+
+        System.out.println("Enter age: ");
+        age = sc.nextInt();
+
+        System.out.println("Enter salary: ");
+        salary = sc.nextInt();
 
         //Print out variables to console
         System.out.println(initial + " " + surname + " is " + age + " years old and is currently " + isEmployed +
