4c224e1c8e95ebe93bc5500346faf40d8fbf5788
ebuchanan2 <Buchanan-E2@ulster.ac.uk>
2024-09-25 15:50:08 +0100

    Added question2

---
+++ src/Question2.java
@@ -0,0 +1,23 @@
+public class Question2 {
+    public static void main(String[] args){
+        //Initialises variables
+        int a = 10;
+        int b = 5;
+        int sum;
+        int difference;
+        int product;
+        double quotient;
+
+        //Does calculations
+        sum = a + b;
+        difference = a - b;
+        product = a * b;
+        quotient = a / b;
+
+        //Prints out results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference : " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
