3092f0a14d1872b3055012682d92929372cd2952
ebuchanan2 <Buchanan-E2@ulster.ac.uk>
2024-09-25 15:24:16 +0100

    Added ques1 class

---
+++ src/question1.java
@@ -0,0 +1,15 @@
+import uulib.*;
+public class question1 {
+    public static void main(String[] args){
+        //Initialise variables
+        char initial = 'J';
+        String surname = "Smith";
+        int age = 25;
+        String isEmployed = "employed";
+        int salary = 21000;
+
+        //Print out variables to console
+        System.out.println(initial + " " + surname + " is " + age + " years old and is currently " + isEmployed +
+                " and has a salary of £" + salary + " per year");
+    }
+}
